0638530dd9d0ade4dc3bc36012846a3bf5bc4053
eric-ward <eric.ward@noaa.gov>
2016-07-21 12:36:41 -0700

    fixed projection code for WRF model output
    - used exported grid from NCAR’s WRF viewer to create lat / lon grid of
    lambert coordinates
    - projected these coordinates to LL and UTM

--- analysis/0-download-data_uwcig.R
+++ analysis/0-download-data_uwcig.R
@@ -1,11 +1,13 @@
-# Download data from World Oceans Atlas
-# URL http://data.nodc.noaa.gov/woa/WOA13/
+# Import example ncdf file from UW CIG model projections
+# and convert WRF model to proper coordinates
 
 base_url <- "http://cses.washington.edu/rocinante/WRF/ECHAM5_A1B/sfc_vars/"
 library(ncdf4)
 library(RNetCDF)
 library(dplyr)
 library(PBSmapping)
+library(sp)
+library(rgdal)
 # Unfortunately ncdf4 doesn't work with URLs, so seems like we have to
 # 1. identify dates w/trawl survey (month / day / year)
 # 2. download only relevant files + process
@@ -32,9 +34,6 @@ download.file(url=fileName, destfile=localName)
 fid<-open.nc(localName)
 dat<-read.nc(fid)
 dat$SST = as.data.frame(dat$SST) - 273.15 # convert to C
-# 162 x 123, on 12km grid
-CEN_LAT = 5058170.29# 45.6427993774414
-CEN_LON = 281932.21 #-119.798309326172
 
 # Read in the longitude latitude that was output from the
 # NCAR IDV viewer. Projection attributes in lambert, with parameters
@@ -44,26 +43,36 @@ lon_lambert = read.nc(fid)$x
 lat_lambert = read.nc(fid)$y
 print.nc(fid)
 
-# convert 
+# grid of all possible values
 xy <- expand.grid("X"=lon_lambert, "Y"=lat_lambert)
-library(sp)
-library(rgdal)
+
 crs <- CRS("+proj=lcc +lat_1=30 +lat_2=60 +lat_0=45.66558 +lon_0=-121 +datum=WGS84 +units=km")
 p <- SpatialPoints(xy, proj4string=crs)
 xy.ll <- as.data.frame(coordinates(spTransform(p, CRS("+proj=longlat +datum=WGS84"))))
-names(xy.ll) = c("lon", "lat")
+names(xy.ll) = c("X", "Y")
 xy.ll$sst = c(as.matrix(dat$SST))
-  
+xy.ll$PID = 1
+xy.ll$POS = seq(1,nrow(xy.ll))
+attr(xy.ll, "zone") = 7
+attr(xy.ll, "projection") = "LL"
+xy.utm = convUL(xy.ll)  
+
+
+### PLOTS 
 data(nepacLL)
-
 plotMap(nepacLL, xlim=c(-135, -105), ylim=c(36, 55))
-points(xy.ll$lon, xy.ll$lat, cex=0.1, col="red")
+points(xy.ll$X, xy.ll$Y, cex=0.2, col = rgb(0, 0, xy.ll$sst, alpha=xy.ll$sst, maxColorValue=25))
 # add trawl data
-points(trawlDat$Lon_mid, trawlDat$Lat_mid, cex=0.1, col="blue")
+points(trawlDat$Lon_mid, trawlDat$Lat_mid, cex=0.1, col=rgb(1,0,0,0.3))
 
-# Getting closer on projections
+# Plot same map in UTM
+plotMap(convUL(nepacLL), xlim = range(xy.utm$X), ylim = range(xy.utm$Y))
+points(xy.utm$X, xy.utm$Y, cex=0.2, col = rgb(0, 0, xy.ll$sst, alpha=xy.ll$sst, maxColorValue=25))
+
+# Getting closer on projections -- something was just not right with the image() call below
 plotMap(nepacLL, xlim=c(-135, -105), ylim=c(36, 55))
-image(xy.ll$lon[xy$Y == min(xy$Y)], xy.ll$lat[xy$X == min(xy$X)], as.matrix(dat$SST), add=T)
-for(i in 1:length(unique(nepacLL$PID))) {
-  polygon(x = nepacLL$X[nepacLL$PID == unique(nepacLL$PID)[i]], y = nepacLL$Y[nepacLL$PID == unique(nepacLL$PID)[i]])
-}
+points(xy.ll$lon, xy.ll$lat, col = rgb(0, 0, xy.ll$sst, alpha=xy.ll$sst, maxColorValue=25), cex=0.2)
+#image(xy.ll$lon[xy$Y == min(xy$Y)], xy.ll$lat[xy$X == min(xy$X)], as.matrix(dat$SST), add=T)
+#for(i in 1:length(unique(nepacLL$PID))) {
+#  polygon(x = nepacLL$X[nepacLL$PID == unique(nepacLL$PID)[i]], y = nepacLL$Y[nepacLL$PID == unique(nepacLL$PID)[i]])
+#}
