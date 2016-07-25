35e596b9962f0c0ef14539aa3932c63a44ab03a1
Sean Anderson <sean@seananderson.ca>
2016-07-25 10:32:57 -0700

    Add figs

--- analysis/3-model-bottom-sst.R
+++ analysis/3-model-bottom-sst.R
@@ -1,6 +1,7 @@
 library(dplyr)
 trawlDat <- readRDS("data-generated/trawl-with-uwcig-sst.rds")
 names(trawlDat)
+dir.create("figs", showWarnings = FALSE)
 
 # Look at pred vs obs -- pretty good fit
 dat <- trawlDat[trawlDat$year=="2003",] %>%
@@ -30,6 +31,16 @@ m_gbm1 <- train(temperature_bottom ~ sst + as.numeric(month) + floor_depth +
 print(m_gbm1)
 dat$temp_bottom_gbm <- predict(m_gbm1)
 
+library(gbm)
+m_gbm2 <- gbm(temperature_bottom ~ sst + as.numeric(month) + floor_depth +
+  I(X/1000) + I(Y/1000), interaction.depth = 3, shrinkage = 0.01, n.trees = 3000,
+  data = dat)
+summary(m_gbm2)
+pdf("figs/gbm-bottom.pdf", width = 9, height = 4)
+par(mfrow=c(1,5))
+sapply(seq_len(5), function(i) plot(m_gbm2, i))
+dev.off()
+
 m_gam1 <- mgcv::gam(temperature_bottom ~ s(sst) + 
   s(as.numeric(month), k = 4) + s(I(floor_depth/100)) + 
     ti(I(X/1000)) + ti(I(Y/1000)) +
@@ -38,9 +49,11 @@ m_gam1 <- mgcv::gam(temperature_bottom ~ s(sst) +
     # s(I(X/1000)) + s(I(Y/1000)),
   data = dat)
 
+pdf("figs/gam-bottom.pdf", width = 6, height = 5)
 mgcv::gam.check(m_gam1)
-par(mfrow = c(1, 6))
+par(mfrow = c(2, 3))
 plot(m_gam1)
+dev.off()
 dat$temp_bottom_gam <- predict(m_gam1)
 AIC(m_gam1)
 summary(m_gam1)
@@ -66,13 +79,29 @@ library(ggplot2)
 library(viridis)
 data(nepacLL)
 dat2 = dat
+dat2$resid <- dat2$temperature_bottom - dat2$temp_bottom_gam
 xlim <- range(dat2$X)
 ylim <- range(dat2$Y)
-ggplot(convUL(nepacLL), aes(X, Y, group = PID)) + geom_polygon() +
-  geom_point(data = dat2, aes(X, Y, 
-    colour = temperature_bottom - temp_bottom_gam)) +
-    # colour = temperature_bottom - temp_bottom_gbm)) +
-    # colour = temperature_bottom - temp_bottom_lm)) +
-  # scale_color_viridis() + 
-  scale_colour_gradient2() +
+
+common <- ggplot(convUL(nepacLL), aes(X, Y, group = PID)) + geom_polygon() +
   coord_equal() + xlim(xlim) + ylim(ylim)
+
+p3 <- common +
+  geom_point(data = dat2, aes(X, Y, colour = resid)) +
+  scale_colour_gradient2()
+
+p1 <- common +
+  geom_point(data = dat2, aes(X, Y, colour = temperature_bottom)) +
+  scale_color_viridis(guide = guide_legend(title = "bot.obs"))
+
+p2 <- common +
+  geom_point(data = dat2, aes(X, Y, colour = sst)) +
+  scale_color_viridis()
+
+p4 <- common +
+  geom_point(data = dat2, aes(X, Y, colour = temp_bottom_gam)) +
+  scale_color_viridis(guide = guide_legend(title = "bot.gam"))
+
+pdf("figs/bottom-temp-residuals.pdf", width = 11, height = 5)
+gridExtra::grid.arrange(p2, p1, p4, p3, nrow = 1)
+dev.off()
